b1ac2e7e60633d3afa2d7c38378971efdc66f48b
rayhan <rayhanyuda13@gmail.com>
2024-05-07 17:11:47 +0700

    fix code

--- config/app.php
+++ config/app.php
@@ -186,7 +186,6 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -212,7 +211,6 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\MainController;
 |
 */
 
-Route::prefix('app-aduan')->group(function () {
     Route::get('/', [MainController::class, 'mainView'])
         ->name('view.main.page');
     Route::get('buat/keluhan', [MainController::class, 'createView'])
@@ -47,4 +46,3 @@ Route::prefix('app-aduan')->group(function () {
         ->name('store.complaiment');
     Route::post('update/status/keluhan', [MainController::class, 'approvalComplaint'])
         ->name('approval.status.complaiment');
-});
