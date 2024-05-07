5470d08952d9b191417f3f6e46e8c527e8a7ade6
rayhan <rayhanyuda13@gmail.com>
2024-05-07 15:40:59 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,36 +13,37 @@ use App\Http\Controllers\MainController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::prefix('app-aduan')->group(function () {
+    Route::get('/', [MainController::class, 'mainView'])
+        ->name('view.main.page');
+    Route::get('buat/keluhan', [MainController::class, 'createView'])
+        ->name('view.create.page');
+    Route::get('monitoring', [MainController::class, 'monitoringView'])
+        ->name('view.monitoring.page');
+    Route::get('list/keluhan', [MainController::class, 'listView'])
+        ->name('view.list.page');
 
-Route::get('/', [MainController::class, 'mainView'])
-    ->name('view.main.page');
-Route::get('app-aduan/buat/keluhan', [MainController::class, 'createView'])
-    ->name('view.create.page');
-Route::get('app-aduan/monitoring', [MainController::class, 'monitoringView'])
-    ->name('view.monitoring.page');
-Route::get('app-aduan/list/keluhan', [MainController::class, 'listView'])
-    ->name('view.list.page');
+    Route::prefix('ajax')->group(function () {
+        Route::get('/open', [MainController::class, 'openAjax'])
+            ->name('ajax.open.content');
+        Route::get('/inprogress', [MainController::class, 'inprogressAjax'])
+            ->name('ajax.inprogress.content');
+        Route::get('/reject', [MainController::class, 'rejectAjax'])
+            ->name('ajax.reject.content');
+        Route::get('/done', [MainController::class, 'doneAjax'])
+            ->name('ajax.done.content');
+        Route::get('/detail/keluhan', [MainController::class, 'detailAjax'])
+            ->name('ajax.detail.keluhan.content');
+        Route::get('/monitoring/keluhan', [MainController::class, 'listMonitoringAjax'])
+            ->name('ajax.monitoring.keluhan.content');
+        Route::get('/info/selengkapnya', [MainController::class, 'infoAjax'])
+            ->name('ajax.info.keluhan');
+        Route::get('/pelacakan', [MainController::class, 'trackingAjax'])
+            ->name('ajax.tracking.keluhan');
+    });
 
-Route::prefix('app-aduan/ajax')->group(function () {
-    Route::get('/open', [MainController::class, 'openAjax'])
-        ->name('ajax.open.content');
-    Route::get('/inprogress', [MainController::class, 'inprogressAjax'])
-        ->name('ajax.inprogress.content');
-    Route::get('/reject', [MainController::class, 'rejectAjax'])
-        ->name('ajax.reject.content');
-    Route::get('/done', [MainController::class, 'doneAjax'])
-        ->name('ajax.done.content');
-    Route::get('/detail/keluhan', [MainController::class, 'detailAjax'])
-        ->name('ajax.detail.keluhan.content');
-    Route::get('/monitoring/keluhan', [MainController::class, 'listMonitoringAjax'])
-        ->name('ajax.monitoring.keluhan.content');
-    Route::get('/info/selengkapnya', [MainController::class, 'infoAjax'])
-        ->name('ajax.info.keluhan');
-    Route::get('/pelacakan', [MainController::class, 'trackingAjax'])
-        ->name('ajax.tracking.keluhan');
+    Route::post('create/complaint', [MainController::class, 'storeComplaint'])
+        ->name('store.complaiment');
+    Route::post('update/status/keluhan', [MainController::class, 'approvalComplaint'])
+        ->name('approval.status.complaiment');
 });
-
-Route::post('app-aduan/create/complaint', [MainController::class, 'storeComplaint'])
-    ->name('store.complaiment');
-Route::post('app-aduan/update/status/keluhan', [MainController::class, 'approvalComplaint'])
-    ->name('approval.status.complaiment');
